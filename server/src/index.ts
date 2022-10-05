f54edd987375714ff947d8f7e25944e299aae89a
Nidhi Patel <nidhipatel439@gmail.com>
2022-10-05 18:11:26 -0400

    working chat

---
+++ server/src/index.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import cors from "cors";
+import { Server } from "socket.io";
+import http from "http";
+
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const port = 5000;
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`listion on ${port}`);
+});
